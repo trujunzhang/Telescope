7a4ab24b530537eda6781b6d369d4e9fef87128e
trujunzhang <trujunzhang@gmail.com>
2016-08-29 21:42:28 +0800

    mup.

---
+++ packages/nova-base-components/lib/single/PostSingleHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ListContainer} from "meteor/utilities:react-list-container";
+import Categories from "meteor/nova:categories";
+
+const PostSingleHeader = () => {
+
+    return (
+      <div>
+
+      </div>
+    )
+}
+
+PostSingleHeader.displayName = "PostSingleHeader";
+
+module.exports = PostSingleHeader;
+export default PostSingleHeader;
