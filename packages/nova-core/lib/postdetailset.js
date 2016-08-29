2f2f8c47e14c45bde48b28d7d40270ce92608d65
trujunzhang <trujunzhang@gmail.com>
2016-08-29 13:24:26 +0800

    mup.

--- packages/nova-core/lib/postdetailset.js
+++ packages/nova-core/lib/postdetailset.js
@@ -2,28 +2,21 @@ import PostDetail from "./postdetail.js";
 
 class PostDetailSet {
   constructor() {
-    this.isPopPage = true;
     this.stack = [];
   }
 
   push(postId) {
     this.isPopPage = true;
     this.stack.push(postId);
-    console.log("push:" + this.stack);
 
     return this.fetchPostPage(postId);
   }
 
   lastPage() {
-    // When the current page is the pushed page,
-    // Need to close it(pop) firstly.
-    if (this.isPopPage) {
-      this.stack.pop();
-      this.isPopPage = false;
-    }
     if (this.stack.length == 0) {
       return null;
     }
+    this.stack.pop();
     console.log("pop:" + this.stack);
     var last = this.stack[this.stack.length - 1];
     return last;
